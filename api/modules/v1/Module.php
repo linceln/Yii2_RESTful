ba9a1e49daa0d77648f3594805e5946598a330f1
linceln <lince2030@gmail.com>
2017-08-31 23:30:59 +0800

    20170831

--- api/modules/v1/Module.php
+++ api/modules/v1/Module.php
@@ -12,11 +12,6 @@ class Module extends \yii\base\Module
      */
     public $controllerNamespace = 'api\modules\v1\controllers';
 
-
-    public function behaviors()
-    {
-    }
-
     /**
      * @inheritdoc
      */
