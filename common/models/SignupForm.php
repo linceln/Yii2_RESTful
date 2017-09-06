3b8cf334303b89b87b156247bacf412a4ce25114
linceln <LIN2016ce>
2017-09-06 14:03:18 +0800

    注册

--- common/models/SignupForm.php
+++ common/models/SignupForm.php
@@ -54,10 +54,10 @@ class SignupForm extends Model
 
         $user = new User();
         $user->username = $this->username;
-        $user->email = $this->mobile;
+        $user->mobile = $this->mobile;
         $user->setPassword($this->password);
         $user->generateAuthKey();
 
-        return $user->save(false) ? $user : null;
+        return $user->save() ? $user : null;
     }
 }
--- common/models/User.php
+++ common/models/User.php
@@ -17,7 +17,6 @@ use yii\web\UnauthorizedHttpException;
  * @property string $username
  * @property string $password_hash
  * @property string $password_reset_token
- * @property string $email
  * @property string $auth_key
  * @property integer $status
  * @property integer $created_at
@@ -58,7 +57,6 @@ class User extends ActiveRecord implements IdentityInterface
             'auth_key' => 'Auth Key',
             'password_hash' => 'Password Hash',
             'password_reset_token' => 'Password Reset Token',
-            'email' => 'Email',
             'status' => 'Status',
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
@@ -73,7 +71,7 @@ class User extends ActiveRecord implements IdentityInterface
         return [
             [['mobile', 'username', 'auth_key', 'password_hash'], 'required'],
             [['status'], 'integer'],
-            [['username', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
+            [['username', 'password_hash', 'password_reset_token'], 'string', 'max' => 255],
             [['auth_key'], 'string', 'max' => 32],
             [['username', 'mobile'], 'unique'],
             [['mobile'], 'string', 'max' => 11],
