ac3864debe61618d05aaf887928573f59a87ee30
linceln <lince2030@gmail.com>
2017-09-15 21:47:33 +0800

    api

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -8,19 +8,29 @@
 
 namespace api\modules\v1\controllers;
 
-
+use Yii;
 use yii\rest\Controller;
 
 class SiteController extends Controller
 {
     public function actionAutoPull()
     {
-        $result = shell_exec('cd ../../ && git pull origin master 2>&1');
-        return [
-            'code' => 1,
-            'msg' => 'Successful',
-            'result' => $result,
-        ];
+        $headers = Yii::$app->request->headers;
+        $signature = $headers->get('X-Hub-Signature');
+//        if ($signature == 'qwertyuiop') {
+            $result = shell_exec('cd ../../ && git pull origin master 2>&1');
+            return [
+                'code' => 1,
+                'msg' => 'Successful',
+                'result' => $result,
+                'signature' => $signature,
+            ];
+//        } else {
+//            return [
+//                'code' => 0,
+//                'msg' => 'Signature is not right',
+//            ];
+//        }
     }
 
     public function actionTest()
