3b2c3a02efbcc94f6ad05cd9f6549499d7ed65d3
linceln <lince2030@gmail.com>
2017-09-15 20:36:10 +0800

    api

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -28,7 +28,7 @@ class SiteController extends Controller
         $result = shell_exec('cd ../../ && git push origin master 2>&1');
         return [
             'code' => 1,
-            'msg' => 'Test is successful now! ',
+            'msg' => 'Test is successful',
             'result' => $result,
         ];
     }
