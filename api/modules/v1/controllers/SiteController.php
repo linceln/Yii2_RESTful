e1abbe6a603b98287afce4407cb675ff9a210557
linceln <lince2030@gmail.com>
2017-09-16 11:04:31 +0800

    api

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -16,10 +16,11 @@ class SiteController extends Controller
     public function actionAutoPull()
     {
         $signature = Yii::$app->request->headers->get('X-Hub-Signature');
+        list($algo, $hash) = explode('=', $signature, 2);
         $payload = file_get_contents('php://input');
 
-        $mySignature = 'sha1=' . hash_hmac('sha1', $payload, Yii::$app->params['githubWebhookSecret']);
-        if (hash_equals($mySignature, $signature)) {
+        $myHash = hash_hmac($algo, $payload, Yii::$app->params['githubWebhookSecret']);
+        if (hash_equals($myHash, $hash)) {
             $result = shell_exec('cd ../../ && git pull origin master 2>&1');
             return [
                 'code' => 1,
