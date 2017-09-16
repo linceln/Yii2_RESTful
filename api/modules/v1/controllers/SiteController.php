7efd93b261c69f8dafcb020cbbfe28726f130442
linceln <lince2030@gmail.com>
2017-09-16 10:46:06 +0800

    api

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -16,19 +16,24 @@ class SiteController extends Controller
     public function actionAutoPull()
     {
         $signature = Yii::$app->request->headers->get('X-Hub-Signature');
-//        $payload = Yii::$app->request->post('payload');
         $payload = file_get_contents('php://input');
-        $mySignature = 'sha1=' . hash_hmac('sha1', $payload, 'http://117.48.203.197:8090/v1/sites/auto-pull');
 
-        $result = shell_exec('cd ../../ && git pull origin master 2>&1');
-        return [
-            'code' => 1,
-            'msg' => 'Successful',
-            'result' => $result,
-            'signature' => $signature,
-            'mySignature' => $mySignature,
-            'payload' => $payload
-        ];
+        $mySignature = 'sha1=' . hash_hmac('sha1', $payload, 'http://117.48.203.197:8090/v1/sites/auto-pull');
+        if (hash_equals($mySignature, $signature)) {
+            $result = shell_exec('cd ../../ && git pull origin master 2>&1');
+            return [
+                'code' => 1,
+                'msg' => 'Successful',
+                'result' => $result,
+                'signature' => $signature,
+                'mySignature' => $mySignature,
+            ];
+        } else {
+            return [
+                'code' => 0,
+                'msg' => 'Bad signature.',
+            ];
+        }
     }
 
     public function actionTest()
