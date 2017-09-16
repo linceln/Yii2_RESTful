a3db0a8402d89eba9cc3f7cfbe52274464b9b6b3
linceln <lince2030@gmail.com>
2017-09-16 10:56:00 +0800

    api

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -18,7 +18,7 @@ class SiteController extends Controller
         $signature = Yii::$app->request->headers->get('X-Hub-Signature');
         $payload = file_get_contents('php://input');
 
-        $mySignature = 'sha1=' . hash_hmac('sha1', $payload, 'http://117.48.203.197:8090/v1/sites/auto-pull');
+        $mySignature = 'sha1=' . hash_hmac('sha1', $payload, Yii::$app->params['githubWebhookSecret']);
         if (hash_equals($mySignature, $signature)) {
             $result = shell_exec('cd ../../ && git pull origin master 2>&1');
             return [
--- backend/config/params.php
+++ backend/config/params.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'githubWebhookSecret' => 'http://117.48.203.197:8090/v1/sites/auto-pull',
 ];
