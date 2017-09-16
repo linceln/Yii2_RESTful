88de6382dda93a8f3dda93be932b949b43a7743c
linceln <lince2030@gmail.com>
2017-09-16 10:04:48 +0800

    api

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -34,7 +34,7 @@ class SiteController extends Controller
     {
         return [
             'code' => 1,
-            'msg' => 'Request is successful now.',
+            'msg' => 'Request is successful.',
         ];
     }
 
