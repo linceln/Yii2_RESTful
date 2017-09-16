b9eacc4124b4df1fec9dbb31b3de725b8c264e3f
linceln <lince2030@gmail.com>
2017-09-16 10:58:26 +0800

    api

--- api/config/params.php
+++ api/config/params.php
@@ -2,4 +2,5 @@
 return [
     'adminEmail' => 'admin@example.com',
     'user.accessTokenExpire' => 7 * 24 * 3600,
+    'githubWebhookSecret' => 'http://117.48.203.197:8090/v1/sites/auto-pull',
 ];
--- backend/config/params.php
+++ backend/config/params.php
@@ -1,5 +1,4 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'githubWebhookSecret' => 'http://117.48.203.197:8090/v1/sites/auto-pull',
 ];
