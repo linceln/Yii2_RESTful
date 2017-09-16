329ec0e09a2408b0ad535b19ced9bcd7a4b73689
linceln <lince2030@gmail.com>
2017-09-16 09:44:39 +0800

    api

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -8,20 +8,24 @@
 
 namespace api\modules\v1\controllers;
 
+use Yii;
 use yii\rest\Controller;
 
 class SiteController extends Controller
 {
     public function actionAutoPull()
     {
-//        $headers = Yii::$app->request->headers;
-//        $signature = $headers->get('X-Hub-Signature');
-            $result = shell_exec('cd ../../ && git pull origin master 2>&1');
-            return [
-                'code' => 1,
-                'msg' => 'Successful',
-                'result' => $result,
-            ];
+        $headers = Yii::$app->request->headers;
+        $signature = $headers->get('X-Hub-Signature');
+        $payload = Yii::$app->request->post('Payload');
+        $result = shell_exec('cd ../../ && git pull origin master 2>&1');
+        return [
+            'code' => 1,
+            'msg' => 'Successful',
+            'result' => $result,
+            'payload' => $payload,
+            'signature' => $signature,
+        ];
     }
 
     public function actionTest()
