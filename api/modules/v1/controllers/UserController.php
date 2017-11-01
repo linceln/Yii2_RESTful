47016199ff4c33d66a57bea838760961a5f4a544
linceln <1>
2017-11-01 11:30:03 +0800

    Users

--- api/modules/v1/controllers/UserController.php
+++ api/modules/v1/controllers/UserController.php
@@ -72,7 +72,7 @@ class UserController extends Controller
     public function actionIndex($page)
     {
         $dataProvider = User::index($page);
-        var_dump($dataProvider->query->asArray()->all());die;
+        var_dump($dataProvider->models->asArray());die;
         return [
             'code' => 1,
             'pages' => $dataProvider->pagination->pageCount,
