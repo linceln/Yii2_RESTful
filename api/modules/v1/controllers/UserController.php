e11f37c666ea1a01d8bca976ff439100e98f6af2
linceln <1>
2017-11-01 13:38:51 +0800

    Users

--- api/modules/v1/controllers/UserController.php
+++ api/modules/v1/controllers/UserController.php
@@ -72,19 +72,19 @@ class UserController extends Controller
     public function actionIndex($page)
     {
         $dataProvider = User::index($page);
-        var_dump(ArrayHelper::toArray($dataProvider->models[0]));
-        die;
-        foreach ($dataProvider->models as $key => $value) {
-            var_dump('~~~~~~~~~~start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-            var_dump($key);
-            var_dump($value);
-            var_dump('~~~~~~~~~~end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-        }
-        die;
+//        var_dump(ArrayHelper::toArray($dataProvider->models[0]));
+//        die;
+//        foreach ($dataProvider->models as $key => $value) {
+//            var_dump('~~~~~~~~~~start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+//            var_dump($key);
+//            var_dump($value);
+//            var_dump('~~~~~~~~~~end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+//        }
+//        die;
         return [
             'code' => 1,
             'pages' => $dataProvider->pagination->pageCount,
-            'users' => $dataProvider->models,
+            'users' => $dataProvider->models[0],
         ];
     }
 
