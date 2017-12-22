52b2e6a0ad69510249f62c1db4e56f8c1825521d
linceln <1>
2017-12-22 18:07:39 +0800

    XDebug test

--- api/modules/v1/controllers/SiteController.php
+++ api/modules/v1/controllers/SiteController.php
@@ -18,22 +18,19 @@ class SiteController extends Controller
     public function actionAutoPull()
     {
         $signature = Yii::$app->request->headers->get('X-Hub-Signature');
-        if(!$signature){
+        if (!$signature) {
             throw new Exception("Bad signature.");
         }
-        $result = explode('=', $signature, 2);
-        if (!empty($result)) {
-            list($algorithm, $hash) = explode('=', $signature, 2);
-            $payload = file_get_contents('php://input');
-            $myHash = hash_hmac($algorithm, $payload, Yii::$app->params['githubWebhookSecret']);
-            if (hash_equals($myHash, $hash)) {
-                $result = shell_exec('cd ../../ && git pull origin master 2>&1');
-                return [
-                    'code' => 1,
-                    'msg' => 'Successful',
-                    'result' => $result,
-                ];
-            }
+        list($algorithm, $hash) = explode('=', $signature, 2);
+        $payload = file_get_contents('php://input');
+        $myHash = hash_hmac($algorithm, $payload, Yii::$app->params['githubWebhookSecret']);
+        if (hash_equals($myHash, $hash)) {
+            $result = shell_exec('cd ../../ && git pull origin master 2>&1');
+            return [
+                'code' => 1,
+                'msg' => 'Successful',
+                'result' => $result,
+            ];
         }
         return [
             'code' => 0,
